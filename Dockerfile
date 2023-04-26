867d5d85aab94be3dba797729ea6fbb45e7579c8
Denis <mistrny@gmail.com>
2023-04-26 23:45:10 +0300

    inference!

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,12 @@ RUN apt-get update &&\
 COPY requirements.txt ./requirements.txt
 SHELL ["/bin/bash", "-c"]
 RUN source /venv/bin/activate && \
+    pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
+RUN source /venv/bin/activate && \
     pip install -r requirements.txt && \
-    pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116 && \
-    apt install -y libsndfile1 && pip install numba==0.48
+    apt install -y libsndfile1
 
 WORKDIR /workspace
 ENV CUBLAS_WORKSPACE_CONFIG=:4096:8
 ENV  PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-CMD ["/venv/bin/python","train.py"]
+ENV PATH="/venv/bin:$PATH"
--- inference.py
+++ inference.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
+import torch
 import numpy as np
+import os
 from utils.aggregation import aggregate_data
 from utils.calculate_features import load_features
 import hydra
 from os import listdir
+from hydra.utils import instantiate
 from os.path import isfile, join
 
 
@@ -24,15 +27,27 @@ def processing(cfg) -> None:
 
     wavs_names = [f for f in listdir(wavs_folder) if isfile(join(wavs_folder, f))]
     load_features(
-        wavs_path=wavs_folder,
+        wavs_path=Path(wavs_folder),
         wavs_names=wavs_names,
-        result_dir=features_folder,
+        result_dir=Path(features_folder),
         dataset_name="inference data",
         recalculate_feature=True,
     )
-
-    aggregate_data(features_folder.parent, features_folder, use_tsv=False, threshold=threshold)
-
+    
+    labels = {0:'angry',1:'sad',2:'neutral',3:'happy'}
+    names_to_id = {}
+    test_samples = []
+    for idx,file in enumerate((Path(features_folder) / "features").glob("*")):
+        test_samples.append(np.load(file))
+        names_to_id[idx] = file.stem
+    model = instantiate(cfg.model)
+    model.load_state_dict(torch.load(cfg.best_model_folder + "/" + cfg.model["_target_"].split(".")[-1]))
+    model.eval()
+    with torch.no_grad():
+        for idx, feature in enumerate(test_samples):
+            output_label = torch.argmax(model(torch.tensor(feature[None, :]))).item()
+            print(f'file {names_to_id[idx]}: prediction: {labels[output_label]}')
+        
 
 if __name__ == "__main__":
     processing()  # pylint: disable=no-value-for-parameter
